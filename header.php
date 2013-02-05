feff5855151eea93a7f2aa1d75af8a4743a75751
Milton Jackson <milton@wearemjc.com>
2013-02-05 12:09:49 -0500

    Adding Glyphs and Icons

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
 	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<? wp_head() ?>
     <link rel="stylesheet" type="text/css" media="all" href="<? bloginfo('template_url') ?>/assets/css/bootstrap.css" />
     <link rel="stylesheet" type="text/css" media="all" href="<? bloginfo('template_url') ?>/assets/css/bootstrap-responsive.css" />
@@ -13,7 +14,7 @@
     <script type="text/javascript" src="<? bloginfo('template_url') ?>/assets/js/modernizr.js" /></script>
 	
 	<!-- IOS Icons -->
-	<link rel="apple-touch-icon-precomposed" href="assets/img/36x36.png">
+	<link rel="apple-touch-icon-precomposed" href="assets/img/icons/36x36.png">
 	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/img/icons/72x72.png">
 	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/img/icons/114x114.png">
 	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/img/icons/144x144.png">
@@ -21,6 +22,7 @@
 	
 	<title><? if (function_exists('is_tag') && is_tag()) { echo 'Tag Archive for &quot;'.$tag.'&quot; - '; } elseif (is_archive()) { wp_title(''); echo ' Archive - '; } elseif (is_search()) { echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; } elseif (!(is_404()) && (is_single()) || (is_page())) { wp_title(''); echo ' - '; } elseif (is_404()) { echo 'Not Found - '; } if (is_home()) { bloginfo('name'); echo ' - '; bloginfo('description'); } else { bloginfo('name'); } ?></title>
 </head>
+
 <body <? body_class() ?>>
 	
 	<!-- start header -->
